b6d155f2aff888ba73a67af2e418f8ee2761b46f
hansani111 <hansanideshmukh894@gmail.com>
2022-12-25 22:18:17 +0530

    added: initial position

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -2,7 +2,17 @@ package com.bridgelabz;
 
 public class SnakeAndLadder {
 
+    static final int STARTING_POSITION = 0;
+    static final int WINING_POSITION = 100;
+
+    static final int NO_PLAY = 0;
+    static final int IS_SNAKE = 1;
+    static final int IS_LADDER = 2;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Problems");
+
+        int playerPosition = 0;
+        System.out.println("initial position is : " + playerPosition);
     }
 }
