f2adc71756acc64261ebf048bad71ea888303b80
hansani111 <hansanideshmukh894@gmail.com>
2022-12-25 22:26:51 +0530

    added: die roll between 1 to 6

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -9,10 +9,18 @@ public class SnakeAndLadder {
     static final int IS_SNAKE = 1;
     static final int IS_LADDER = 2;
 
+    static int toGetDieRollNumber(){
+        int randomCheck = (int) (1 + Math.random() * 6);
+        return  randomCheck;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Problems");
 
-        int playerPosition = 0;
-        System.out.println("initial position is : " + playerPosition);
+        int playerPosition =0;
+        System.out.println("initial position is : "+playerPosition);
+
+        int randomCheck = toGetDieRollNumber();
+        System.out.println("number between 1 to 6 : "+randomCheck);
     }
 }
