78fcd343df195ac490530ee868a64c19cd05feb0
hansani111 <110657880+hansani111@users.noreply.github.com>
2022-12-26 15:18:16 +0530

    Update SnakeAndLadder.java

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -3,12 +3,7 @@ package com.bridgelabz;
 public class SnakeAndLadder {
 
     static final int STARTING_POSITION = 0;
-    static final int WINING_POSITION = 100;
-
-    static final int NO_PLAY = 0;
-    static final int IS_SNAKE = 1;
-    static final int IS_LADDER = 2;
-
+    
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Problems");
 
